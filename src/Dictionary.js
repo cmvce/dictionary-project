6502fd013f5304421c7fabcd215417045e85f005
Coleene <acevedocoleene@gmail.com>
2025-02-28 14:52:48 +0100

    Added API call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-
+import "./Dictionary.css";
+import axios from "axios";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function getWord(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    const key = "80oc158tb64caae306c6eb4bf7cef14f";
+    const url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    axios.get(url).then(getWord);
   }
 
   function updateKeyword(event) {
@@ -16,10 +24,11 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input
           type='search'
-          autofocus={true}
+          autoFocus={true}
           placeholder='Search a word....'
           onChange={updateKeyword}
         />
+        <button className='btn'>Search</button>
       </form>
     </div>
   );
