3ebf154e4bd9715bfe30e9cb369d5fca4ae83e42
Coleene <acevedocoleene@gmail.com>
2025-03-05 16:24:06 +0100

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   if (props.meaning.definition[0]) {
     return (
-      <div className='Meaning'>
+      <div className='Meaning '>
         <h3>{props.meaning.partOfSpeech}</h3>
 
-        <p className='definition'> Definition:</p>
+        <p className='definition '> Definition:</p>
         <p>{props.meaning.definition}</p>
         <br />
         <p className='example'>Example:</p>
         <p>{props.meaning.example}</p>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </div>
     );
   }
---
+++ src/Synonyms.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className='Synonyms'>
+        {props.synonyms.map(function (synonyms, index) {
+          return <li key={index}>{synonyms}</li>;
+        })}
+      </div>
+    );
+  }
+}
